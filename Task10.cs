98caf185515484f532c2009848e9ebc98c4ee5d1
maria <smolkina90@yandex.ru>
2022-11-26 12:45:48 +0500

    Программа проверена

--- Task10.cs
+++ Task10.cs
@@ -1,1 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введи трехзначное число:");
+int SddigitinNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(SddigitinNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);

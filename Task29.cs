d86ef57cb96b4769099dd7e4a232b9ce07bbdd74
Medica90 <87068457+Medica90@users.noreply.github.com>
2022-12-07 19:39:27 +0500

    Add files via upload

---
+++ Task29.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+int [] array = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+ {
+    array [i] = new Random().Next(0, 100);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return array[a];
+}
